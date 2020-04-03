1ca60efa6c2d300a63b31d6d82a8a8d70d0d8911
Erick Medina <erickmedinabe@gmail.com>
2020-04-03 14:59:26 +0200

    sass added to the project and navbar style changed

---
+++ contact.php
@@ -0,0 +1,40 @@
+<?php
+
+// validate input
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $mailFrom = $_POST['mail'];
+    $phone = $_POST['phone'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    // email receiver
+    $mailTo = "erickmedinabe@gmail.com";
+    // who the mail is from
+    $headers = "From: ". $mailFrom;
+    // message sent
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+    // show successful message to user
+    if (mail($mailTo, $subject, $txt, $headers)) {
+        echo "<h1>Sent successfully! Thank you"." ".$name.", We will contact you shortly</h1>";
+
+    } else {
+      echo 'Something went wrong!';
+    }
+
+
+        /*
+    mail($mailTo, $subject, $txt, $headers);
+    // function to take us back to the homepage
+    header("Location: contact.html?mailsend");
+
+require('contact.html');
+        */
+}
+
+
+
+
+
+
