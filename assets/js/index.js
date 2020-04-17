fbb84d7b9d931865ac42eb7bdc54eb377e611621
Erick Medina <erickmedinabe@gmail.com>
2020-04-17 12:01:36 +0200

    update on partners and general site

---
+++ assets/js/index.js
@@ -0,0 +1,76 @@
+// LINKS
+
+document.getElementById('links').innerHTML =
+    '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"\n' +
+    '          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">\n' +
+    '    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;1,300&display=swap" rel="stylesheet">\n' +
+    '    <link href="https://fonts.googleapis.com/css2?family=Gotu&display=swap" rel="stylesheet">\n' +
+    '    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">'
+
+// NAVBAR HTML
+document.getElementById('navMenu').innerHTML =
+    '<nav class="navbar navbar-expand-sm py-4 navbar-light fixed-top" id="nav-bar">' +
+    '    <div class="container">' +
+    '        <a class="navbar-brand" id="logo-font" href="#" alt="">ONW LOGO</a>' +
+    '        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarNav">' +
+    '            <span id="some" class="navbar-toggler-icon"></span>' +
+    '        </button>' +
+    '        <div class="collapse navbar-collapse text-center" id="navbarNav">' +
+    '            <ul class="navbar-nav ml-auto">' +
+    '                <li class="nav-item">' +
+    '                    <a class="nav-link mr-xl-4" href="index.html">Home</a>' +
+    '                </li>' +
+    '                <li class="nav-item">' +
+    '                    <a class="nav-link mr-xl-4" href="traject.html">Traject</a>' +
+    '                </li>' +
+    '                <li class="nav-item">' +
+    '                    <a class="nav-link mr-xl-4" href="situering.html">Situering</a>' +
+    '                </li>' +
+    '                <li class="nav-item">' +
+    '                    <a class="nav-link mr-xl-4" href="wieiswie.html">Wie is wie</a>' +
+    '                </li>' +
+    '                <li class="nav-item">' +
+    '                    <a class="nav-link mr-xl-4" href="partners.html">Partners</a>\n' +
+    '                </li>' +
+    '                <li class="nav-item">' +
+    '                    <a class="nav-link mr-xl-4" href="vacatures.html">Vacatures</a>' +
+    '                </li>\n' +
+    '                <li class="nav-item">' +
+    '                    <a class="nav-link" href="contact.html">Contact</a>' +
+    '                </li>' +
+    '            </ul>' +
+    '        </div>' +
+    '    </div>' +
+    '</nav>';
+
+// FOOTER
+document.getElementById('footer').innerHTML =
+    '<footer id="main-footer" class="text-center mt-5 p-4">\n' +
+    '    <div class="container">\n' +
+    '        <div class="row">\n' +
+    '            <div class="col-xl-6 col-lg-6 align-self-center">\n' +
+    '                <h3 class="footer-title mb-4">Vind-ons-leuk op Facebook!</h3>\n' +
+    '                <a id="btn" href="https://www.facebook.com/Ondersteuningsnetwerk-West-106662227648329/" target="_blank">\n' +
+    '                    <i class="fa fa-facebook mt-2"></i>\n' +
+    '                </a>\n' +
+    '            </div>\n' +
+    '            <div class="col-xl-6 col-lg-6">\n' +
+    '                <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FOndersteuningsnetwerk-West-106662227648329%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"\n' +
+    '                        width="380" height="300"\n' +
+    '                        style="border-left:20px solid #000; border-right:20px solid #000; border-top:40px solid #000; border-bottom:40px solid #000; border-radius:25px; box-shadow:0 10px 15px #888;  overflow:hidden"\n' +
+    '                        scrolling="no" frameborder="0"\n' +
+    '                        allowTransparency="true" allow="encrypted-media"></iframe>\n' +
+    '            </div>\n' +
+    '        </div>\n' +
+    '    </div>\n' +
+    '</footer>\n' +
+    '<footer id="second-footer" class="text-center">\n' +
+    '    <div class="container">\n' +
+    '        <div class="row">\n' +
+    '            <div class="col">\n' +
+    '                <h6 id="text-footer">Copyright &copy;\n' +
+    '                    <span id="year"></span> ONW - Developed by Erick Medina</h6>\n' +
+    '            </div>\n' +
+    '        </div>\n' +
+    '    </div>\n' +
+    '</footer>';
---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+function clickOnTitle() {
+    document.getElementById('title-x-puzzle').style.color = 'red';
+
+}
+
+// Buitengewoon onderwijs
+
+// When clicking, display text
+
+function clickonBO() {
+    let buitenOnderwijs = document.getElementById('b-onderwijs');
+    buitenOnderwijs.style.display = 'block';
+
+}
+
+
