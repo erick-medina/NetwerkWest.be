720a7a0656a5304fd80308e32b2b57abc102c7c5
Erick Medina <erickmedinabe@gmail.com>
2020-04-27 18:46:07 +0200

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,11 @@
-// LINKS
-
-document.getElementById('links').innerHTML =
-    '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"\n' +
-    '          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">\n' +
-    '<link href="https://cdn.jsdelivr.net/gh/vaibhav111tandon/vov.css@latest/vov.css" rel="stylesheet" type="text/css">\n' +
-    '<link href="https://fonts.googleapis.com/css2?family=Amatic+SC&display=swap" rel="stylesheet">' +
-    '<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;1,300&display=swap" rel="stylesheet">\n' +
-    '<link href="https://fonts.googleapis.com/css2?family=Baloo+Paaji+2:wght@400;600&display=swap" rel="stylesheet">' +
-    '<link href="https://fonts.googleapis.com/css2?family=Gotu&display=swap" rel="stylesheet">\n' +
-    '<link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">'
-
 // NAVBAR HTML
+
 document.getElementById('navMenu').innerHTML =
     '<nav class="navbar navbar-expand-sm py-4 navbar-light fixed-top" id="nav-bar">' +
     '    <div class="container">' +
     '        <a class="navbar-brand" id="logo-font">' +
     '<img src="../../img/logos/logo-onw.png" alt="" class="d-inline-block align-middle mr-2"></a>  ' +
-    '        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarNav">' +
+    '        <button class="navbar-toggler" aria-label="right align" data-toggle="collapse" data-target="#navbarNav">' +
     '            <span id="some" class="navbar-toggler-icon"></span>' +
     '        </button>' +
     '        <div class="collapse navbar-collapse text-center" id="navbarNav">' +
@@ -48,32 +37,33 @@ document.getElementById('navMenu').innerHTML =
     '</nav>';
 
 // FOOTER
+
 document.getElementById('footer').innerHTML =
     '<footer id="main-footer" class="text-center mt-5 p-4">\n' +
     '    <div class="container">\n' +
     '        <div class="row">\n' +
     '            <div class="col-xl-6 col-lg-6 col-sm-12 align-self-center">\n' +
     '                <h3 class="footer-title mb-4">Vind-ons-leuk op Facebook!</h3>\n' +
-    '                <a id="btn" href="https://www.facebook.com/Ondersteuningsnetwerk-West-106662227648329/" target="_blank">\n' +
+    '                <a id="btn" href="https://www.facebook.com/Ondersteuningsnetwerk-West-106662227648329/" target="_blank" rel="noopener">\n' +
     '                    <i class="fa fa-facebook mt-2"></i>\n' +
     '                </a>\n' +
     '            </div>\n' +
     '            <div id="fb-plugin" class="col-xl-6 col-lg-6 col-sm-12">\n' +
-    '                <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FOndersteuningsnetwerk-West-106662227648329%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"\n' +
+    '                <iframe title="Facebook plugin" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FOndersteuningsnetwerk-West-106662227648329%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"\n' +
     '                        width="380" height="300"\n' +
     '                        style="border-left:20px solid #000; border-right:20px solid #000; border-top:40px solid #000; border-bottom:40px solid #000; border-radius:25px; box-shadow:0 10px 15px #888;  overflow:hidden"\n' +
     '                        scrolling="no" frameborder="0"\n' +
     '                        allowTransparency="true" allow="encrypted-media"></iframe>\n' +
     '</div>\n' +
     '            <div id="fb-plugin-sm" class="col-xl-6 col-lg-6 col-sm-12">\n' +
-    '                <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FOndersteuningsnetwerk-West-106662227648329%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"\n' +
+    '                <iframe title="Facebook plugin" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FOndersteuningsnetwerk-West-106662227648329%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"\n' +
     '                        width="230" height="300"\n' +
     '                        style="border-left:20px solid #000; border-right:20px solid #000; border-top:40px solid #000; border-bottom:40px solid #000; border-radius:25px; box-shadow:0 10px 15px #888;  overflow:hidden"\n' +
     '                        scrolling="no" frameborder="0"\n' +
     '                        allowTransparency="true" allow="encrypted-media"></iframe>\n' +
     '        </div>\n' +
     '            <div id="fb-plugin-md" class="col-xl-6 col-lg-6 col-sm-12 pt-3">\n' +
-    '                <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FOndersteuningsnetwerk-West-106662227648329%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"\n' +
+    '                <iframe title="Facebook plugin" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FOndersteuningsnetwerk-West-106662227648329%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"\n' +
     '                        width="370" height="300"\n' +
     '                        style="border-left:20px solid #000; border-right:20px solid #000; border-top:40px solid #000; border-bottom:40px solid #000; border-radius:25px; box-shadow:0 10px 15px #888;  overflow:hidden"\n' +
     '                        scrolling="no" frameborder="0"\n' +
